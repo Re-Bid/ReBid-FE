37b8c1f6154d70a80f3138f128ee169251726cf0
yunchan312 <phenomenal312@hufs.ac.kr>
2024-07-30 23:00:13 +0900

    [FE-22] 홈 연동

--- src/components/home/HomePhoto.jsx
+++ src/components/home/HomePhoto.jsx
@@ -2,7 +2,7 @@ import { HeartIcon } from "@heroicons/react/24/outline";
 import { useState } from "react";
 import { SwiperSlide } from "swiper/react";
 
-export default function HomePhoto() {
+export default function HomePhoto({ image, itemName }) {
   const [isCursor, setIsCursor] = useState(false);
   const onMouseEvent = () => {
     setIsCursor((prev) => !prev);
@@ -17,13 +17,13 @@ export default function HomePhoto() {
           >
             <HeartIcon className="size-4 text-white self-end" />
             <div className="text-white self-start flex flex-col items-start">
-              <div className="text-[13px]">스타일리시 커피 테이블</div>
-              <div className="font-light text-[12px]">시작가 15,000</div>
+              <div className="text-[13px]">{itemName}</div>
+              {/* <div className="font-light text-[12px]">시작가 15,000</div> */}
             </div>
           </div>
         ) : null}
         <div onMouseEnter={onMouseEvent} className="h-full bg-neutral-200">
-          사진
+          {image}
         </div>
       </div>
     </div>
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -7,39 +7,41 @@ import { Navigation } from "swiper/modules";
 
 import "swiper/css/pagination";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Banner from "../components/home/Banner";
 
 export default function Home() {
-  const auctionArray = [
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-  ];
+  const [data, setData] = useState([]);
+  const [endTod, setEndTod] = useState([]);
+  const [realTime, setRealTime] = useState([]);
 
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_BASE_URL}/bids`)
-      .then((res) => console.log(res))
+      .then((res) => {
+        setData(res.data.data.bids);
+        return res.data.data.bids[0];
+      })
+      .catch((e) => console.log(e));
+
+    axios
+      .get(`${process.env.REACT_APP_BASE_URL}/bids/imminent`)
+      .then((res) => {
+        setEndTod(res.data.data.bids);
+      })
+      .catch((e) => console.log(e));
+
+    axios
+      .get(`${process.env.REACT_APP_BASE_URL}/bids/real-time`)
+      .then((res) => {
+        setRealTime(res.data.data.bids);
+      })
       .catch((e) => console.log(e));
   }, []);
 
   return (
     <div className="flex flex-col items-center">
-      <div className="h-[350px] w-full grid grid-cols-2">
-        <div className="h-full text-white bg-[#88939A] px-14 py-10 flex flex-col justify-around font-light">
-          <div className="font-bold text-xl">
-            7/23(화) 오후 6시부터 응찰 오픈
-          </div>
-          <div>
-            <div className="font-bold">프리뷰 일정</div>
-            <div>온라인: 7/23 (화) 10am 부터 마감전, 24시간 항시</div>
-          </div>
-          <div>
-            7/29 (월) 4pm 부터,
-            <br /> 홈페이지에서 5분 간격 10작품씩 순차 마감
-          </div>
-        </div>
-        <div className="bg-neutral-300">사진</div>
-      </div>
-
+      <Banner bid={data[0]} />
       <div className="flex flex-col gap-5 py-5">
         <div className="text-2xl px-2">지금 진행중인 경매</div>
         <div className="w-[850px] overflow-visible">
@@ -50,13 +52,17 @@ export default function Home() {
             modules={[Navigation]}
             className="overflow-visible"
           >
-            {auctionArray.map((e, el) => {
-              return (
-                <SwiperSlide key={el}>
-                  <HomePhoto />
-                </SwiperSlide>
-              );
-            })}
+            {realTime.length !== 0 ? (
+              realTime.map((e, el) => {
+                return (
+                  <SwiperSlide key={el}>
+                    <HomePhoto image={e.imageUrl} name={e.itemName} />
+                  </SwiperSlide>
+                );
+              })
+            ) : (
+              <div>No Data</div>
+            )}
           </Swiper>
         </div>
       </div>
@@ -71,13 +77,17 @@ export default function Home() {
             modules={[Navigation]}
             className="overflow-visible"
           >
-            {auctionArray.map((e, el) => {
-              return (
-                <SwiperSlide key={el}>
-                  <HomePhoto />
-                </SwiperSlide>
-              );
-            })}
+            {endTod.length !== 0 ? (
+              endTod.map((e, el) => {
+                return (
+                  <SwiperSlide key={el}>
+                    <HomePhoto name={e.itemName} image={e.imageUrl} />
+                  </SwiperSlide>
+                );
+              })
+            ) : (
+              <div>No Data</div>
+            )}
           </Swiper>
         </div>
       </div>
@@ -92,13 +102,17 @@ export default function Home() {
             modules={[Navigation]}
             className="overflow-visible"
           >
-            {auctionArray.map((e, el) => {
-              return (
-                <SwiperSlide key={el}>
-                  <HomePhoto />
-                </SwiperSlide>
-              );
-            })}
+            {data.length !== 0 ? (
+              data.map((e, el) => {
+                return (
+                  <SwiperSlide key={el}>
+                    <HomePhoto image={e.imageUrl} name={e.itemName} />
+                  </SwiperSlide>
+                );
+              })
+            ) : (
+              <div>No Data</div>
+            )}
           </Swiper>
         </div>
       </div>
