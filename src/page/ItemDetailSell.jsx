6f713f7bd4f59e31f616fbce3512554ba1122d2f
juhui88 <sand8594@naver.com>
2024-08-01 07:37:43 +0900

    feat: naverPay 로직 구현

--- src/page/ItemDetailSell.jsx
+++ src/page/ItemDetailSell.jsx
@@ -1,32 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import DetailContainer from '../components/detailcompos/DetailContainer';
+import axios from 'axios';
 
 const ItemDetailSell = () => {
     const { id } = useParams()
-    const data = {
-        bidType: "실시간 경매",
-        productName: "스타일리시 목제 테이블",
-        time: "2024-08-07T16:00",
-        startPrice: "15,000원",
-        nowHighPrice: "20,000원",
-        info: "오래된 나무 팔레트로 만든 이 커피 테이블은 유리 상판과 강철 다리로 러스틱하고 모던한 미학을 결합하였습니다",
-    }
+
+    const [data, setData] = useState([])
+    const [imgUrl, setImgUrl] = useState("");
+    // const data = {
+    //     bidType: "실시간 경매",
+    //     productName: "스타일리시 목제 테이블",
+    //     time: "2024-08-07T16:00",
+    //     startPrice: "15,000원",
+    //     nowHighPrice: "20,000원",
+    //     info: "오래된 나무 팔레트로 만든 이 커피 테이블은 유리 상판과 강철 다리로 러스틱하고 모던한 미학을 결합하였습니다",
+    // }
+    useEffect(() => {
+        axios.get(`${process.env.REACT_APP_BASE_URL}/bids/${id}`).then(res => {
+            console.log(res);
+
+            setData(res.data.data);
+            setImgUrl(res.data.data.imageUrls[0]);
+        }).catch(err => console.log(err))
+    }, [])
     return (
         <div className="py-7">
             <div className="px-10">
 
                 <div className="flex space-x-7 pb-10">
 
-                    <img className="w-[504px] h-[400px]" />
+                    <img className="w-[504px] h-[400px]" src={imgUrl} />
+
                     <DetailContainer
-                        productName={data.productName}
-                        info={data.info}
-                        time={data.time}
+                        bidId={data.bidId}
+                        productName={data.itemName}
+                        time={data.endDate}
                         startPrice={data.startPrice}
+                        nowHighPrice={data.currentPrice}
+                        info={data.itemIntro}
+                        isHeart={data.isHeart}
+                        bidType={data.bidType}
                         isSell
                     />
-
 
                 </div>
             </div>
@@ -36,15 +52,7 @@ const ItemDetailSell = () => {
                     제품 소개
                 </div>
                 <div>
-                    간략한 소개 어쩌구저쩌구
-                    어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구
-                    어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구
-                    어쩌구저쩌구어쩌구저쩌구
-
-                    어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구
-                    어쩌구저쩌구어쩌구저쩌구
-
-                    어쩌구저쩌구어쩌구저쩌구어쩌구저쩌구
+                    {data?.itemDescription}
                 </div>
             </div>
 
